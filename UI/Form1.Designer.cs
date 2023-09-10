97b1a624f538fd854db175f071d6c0e0748738b7
Netz9 <nilmar2024@gmail.com>
2023-09-09 18:35:15 -0600

    .

--- UI/Form1.Designer.cs
+++ UI/Form1.Designer.cs
@@ -47,7 +47,7 @@
             // 
             this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.textBox1.Font = new System.Drawing.Font("Arial", 11.25F);
-            this.textBox1.Location = new System.Drawing.Point(195, 123);
+            this.textBox1.Location = new System.Drawing.Point(338, 209);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(178, 25);
             this.textBox1.TabIndex = 2;
@@ -56,7 +56,7 @@
             // 
             this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.textBox2.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(195, 187);
+            this.textBox2.Location = new System.Drawing.Point(338, 273);
             this.textBox2.Name = "textBox2";
             this.textBox2.PasswordChar = '*';
             this.textBox2.Size = new System.Drawing.Size(178, 25);
@@ -67,7 +67,7 @@
             this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = global::UI.Properties.Resources.dis;
-            this.pictureBox1.Location = new System.Drawing.Point(425, 91);
+            this.pictureBox1.Location = new System.Drawing.Point(568, 177);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(159, 138);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -79,7 +79,7 @@
             this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox2.Image = global::UI.Properties.Resources.Usarios;
-            this.pictureBox2.Location = new System.Drawing.Point(148, 113);
+            this.pictureBox2.Location = new System.Drawing.Point(291, 199);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(41, 36);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -91,7 +91,7 @@
             this.pictureBox3.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox3.Image = global::UI.Properties.Resources._lock;
-            this.pictureBox3.Location = new System.Drawing.Point(148, 177);
+            this.pictureBox3.Location = new System.Drawing.Point(291, 263);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(41, 36);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -103,7 +103,7 @@
             this.Ocultar.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.Ocultar.BackColor = System.Drawing.Color.Transparent;
             this.Ocultar.Image = global::UI.Properties.Resources.eyelashes;
-            this.Ocultar.Location = new System.Drawing.Point(376, 181);
+            this.Ocultar.Location = new System.Drawing.Point(519, 267);
             this.Ocultar.Name = "Ocultar";
             this.Ocultar.Size = new System.Drawing.Size(43, 40);
             this.Ocultar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -116,7 +116,7 @@
             this.pictureBox4.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox4.Image = global::UI.Properties.Resources.eye;
-            this.pictureBox4.Location = new System.Drawing.Point(376, 181);
+            this.pictureBox4.Location = new System.Drawing.Point(519, 267);
             this.pictureBox4.Name = "pictureBox4";
             this.pictureBox4.Size = new System.Drawing.Size(43, 40);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -128,7 +128,7 @@
             // 
             this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.button1.Font = new System.Drawing.Font("Arial", 11.25F);
-            this.button1.Location = new System.Drawing.Point(231, 218);
+            this.button1.Location = new System.Drawing.Point(374, 304);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(104, 53);
             this.button1.TabIndex = 5;
@@ -140,7 +140,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::UI.Properties.Resources.fondo;
-            this.ClientSize = new System.Drawing.Size(696, 396);
+            this.ClientSize = new System.Drawing.Size(983, 568);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
